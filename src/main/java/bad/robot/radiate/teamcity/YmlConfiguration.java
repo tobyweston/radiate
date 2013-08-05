55b6a16b1290f0985bfeb0604e7cfb73c5b9ec6b
toby.weston <toby.weston@gmail.com>
2013-08-05 11:30:33 +0100

    #12 write YML to the file (snakeyml changes the format if it's less than three lines, go figure!)

--- src/main/java/bad/robot/radiate/teamcity/YmlConfiguration.java
+++ src/main/java/bad/robot/radiate/teamcity/YmlConfiguration.java
@@ -3,9 +3,13 @@ package bad.robot.radiate.teamcity;
 import bad.robot.http.HttpClient;
 import bad.robot.radiate.configuration.Configuration;
 import com.googlecode.totallylazy.Callable1;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static bad.robot.http.HttpClients.anApacheClient;
 import static bad.robot.radiate.Environment.getEnvironmentVariable;
@@ -15,15 +19,17 @@ import static org.apache.commons.io.FileUtils.writeStringToFile;
 
 public class YmlConfiguration implements Configuration {
 
+    private final HttpClient http = anApacheClient();
+
     public YmlConfiguration() throws IOException {
-        File configuration = initialiseIfRequired();
+        File configuration = createFileIfRequired("config.yml");
         load(configuration);
     }
 
-    private File initialiseIfRequired() throws IOException {
+    private File createFileIfRequired(String file) throws IOException {
         String home = System.getProperty("user.home");
         new File(home + File.separator + ".radiate").mkdirs();
-        File configuration = new File(home + File.separator + ".radiate" + File.separator + "config.yml");
+        File configuration = new File(home + File.separator + ".radiate" + File.separator + file);
         boolean created = configuration.createNewFile();
         if (created)
             initialise(configuration);
@@ -35,11 +41,19 @@ public class YmlConfiguration implements Configuration {
     }
 
     private void initialise(File configuration) throws IOException {
-        HttpClient http = anApacheClient();
+        Yaml yaml = new Yaml();
+        Map<String, Object> data = new HashMap<>();
+        data.put("projects", getProjectIds());
+        data.put("host", host());
+        data.put("port", port());
+        writeStringToFile(configuration, yaml.dump(data));
+    }
+
+    private List<String> getProjectIds() {
         Server server = new Server(host(), port());
         TeamCity teamcity = new TeamCity(server, http, new JsonProjectsUnmarshaller(), new JsonProjectUnmarshaller(), new JsonBuildUnmarshaller());
         Iterable<Project> projects = teamcity.retrieveProjects();
-        writeStringToFile(configuration, sequence(projects).map(projectAsId()).toString());
+        return sequence(projects).map(projectAsId()).toList();
     }
 
     @Override
