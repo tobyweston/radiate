bf75eaa2aa329dc4d9734eb034bf6d570aab0a52
toby.weston <toby.weston@gmail.com>
2013-09-24 09:31:42 +0100

    print an abbreviated response body when non-json responses are encountered

--- src/main/java/bad/robot/radiate/teamcity/UnexpectedContentType.java
+++ src/main/java/bad/robot/radiate/teamcity/UnexpectedContentType.java
@@ -3,21 +3,20 @@ package bad.robot.radiate.teamcity;
 import bad.robot.http.Header;
 import bad.robot.http.HttpResponse;
 import com.googlecode.totallylazy.Predicate;
+import org.apache.commons.lang3.StringUtils;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static java.lang.String.format;
 
 class UnexpectedContentType extends TeamCityException {
 
-    private final String body;
-
     public UnexpectedContentType(HttpResponse response) {
-        super(format("Unexpected response format '%s' (%s %s) from %s", sequence(response.getHeaders()).find(contentType()).get().value(), response.getStatusCode(), response.getStatusMessage(), response.getOriginatingUri()));
-        this.body = response.getContent().asString();
+        super(format("Unexpected response format '%s' (%s %s) from %s%n%s", sequence(response.getHeaders()).find(contentType()).get().value(), response.getStatusCode(), response.getStatusMessage(), response.getOriginatingUri(), abbreviated(response)));
     }
 
-    public String getBody() {
-        return body;
+    private static String abbreviated(HttpResponse response) {
+        String body = response.getContent().asString();
+        return StringUtils.abbreviate(body, 400);
     }
 
     private static Predicate<Header> contentType() {
