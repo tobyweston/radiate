96433844c911444d5cd500a097edffc4b441b871
toby.weston <toby.weston@db.com>
2013-11-18 10:32:45 +0000

    adding progress object

---
+++ src/main/java/bad/robot/radiate/ui/Progress.java
@@ -0,0 +1,33 @@
+package bad.robot.radiate.ui;
+
+public class Progress {
+
+    private int current;
+    private final double max;
+
+    public Progress(int current, int max) {
+        this.current = current;
+        this.max = max;
+    }
+
+    public void increment() {
+        current++;
+    }
+
+    int asAngle() {
+        return - asPercentageOf(360).intValue();
+    }
+
+    private Double asPercentageOf(int i) {
+        return current / max * i;
+    }
+
+    @Override
+    public String toString() {
+        return asPercentageOf(100).intValue() + "%";
+    }
+
+    boolean complete() {
+        return current >= max;
+    }
+}
---
+++ src/test/java/bad/robot/radiate/ui/ProgressTest.java
@@ -0,0 +1,17 @@
+package bad.robot.radiate.ui;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProgressTest {
+
+    @Test
+    public void stringRepresentation() throws Exception {
+        assertThat(new Progress(20, 100).toString(), is("20%"));
+        assertThat(new Progress(40, 200).toString(), is("20%"));
+        assertThat(new Progress(40, 120).toString(), is("33%"));
+
+    }
+}
