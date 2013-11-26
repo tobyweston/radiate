79da80635720503dfd7c28b09078cae0ecfe7672
tobyweston <toby.weston@db.com>
2013-11-26 07:37:19 +0000

    #13 refactoring towards folds(?)

--- src/main/java/bad/robot/radiate/teamcity/AllProjectsMonitor.java
+++ src/main/java/bad/robot/radiate/teamcity/AllProjectsMonitor.java
@@ -2,13 +2,11 @@ package bad.robot.radiate.teamcity;
 
 import bad.robot.http.HttpClient;
 import bad.robot.radiate.Activity;
-import bad.robot.radiate.Functions;
 import bad.robot.radiate.Status;
 import bad.robot.radiate.monitor.Information;
 import bad.robot.radiate.monitor.MonitoringTask;
 import bad.robot.radiate.monitor.NonRepeatingObservable;
 import com.googlecode.totallylazy.Callable1;
-import com.googlecode.totallylazy.Pair;
 import com.googlecode.totallylazy.Sequence;
 
 import static bad.robot.http.HttpClients.anApacheClient;
@@ -16,8 +14,9 @@ import static bad.robot.http.configuration.HttpTimeout.httpTimeout;
 import static bad.robot.radiate.ActivityAggregator.aggregated;
 import static bad.robot.radiate.Functions.asString;
 import static bad.robot.radiate.StatusAggregator.aggregated;
+import static bad.robot.radiate.teamcity.Build.Functions.toActivity;
+import static bad.robot.radiate.teamcity.Build.Functions.toStatus;
 import static com.google.code.tempusfugit.temporal.Duration.minutes;
-import static com.googlecode.totallylazy.Pair.pair;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static java.lang.String.format;
 
@@ -43,9 +42,9 @@ public class AllProjectsMonitor extends NonRepeatingObservable implements Monito
             Iterable<Project> projects = configuration.filter(teamcity.retrieveProjects());
             monitored = sequence(projects).map(asString());
             Iterable<BuildType> buildTypes = teamcity.retrieveBuildTypes(projects);
-            Sequence<Pair<Status, Activity>> states = sequence(buildTypes).mapConcurrently(toStatusAndActivity(teamcity));
-            Status status = aggregated(states.map(Functions.<Status, Activity>first())).getStatus();
-            Activity activity = aggregated(states.map(Functions.<Status, Activity>second())).getStatus();
+            Sequence<Build> builds = sequence(buildTypes).mapConcurrently(toStatusAndActivity(teamcity));
+            Status status = aggregated(builds.map(toStatus())).getStatus();
+            Activity activity = aggregated(builds.map(toActivity())).getStatus();
             notifyObservers(activity);
             notifyObservers(status);
             notifyObservers(new Information(toString()));
@@ -54,13 +53,12 @@ public class AllProjectsMonitor extends NonRepeatingObservable implements Monito
         }
     }
 
-    private Callable1<BuildType, Pair<Status, Activity>> toStatusAndActivity(final TeamCity teamcity) {
-        return new Callable1<BuildType, Pair<Status, Activity>>() {
+    private Callable1<BuildType, Build> toStatusAndActivity(final TeamCity teamcity) {
+        return new Callable1<BuildType, Build>() {
             @Override
-            public Pair<Status,Activity> call(BuildType buildType) throws Exception {
+            public Build call(BuildType buildType) throws Exception {
                 notifyObservers(new Information(AllProjectsMonitor.this.toString()));
-                Build build = teamcity.retrieveLatestBuild(buildType);
-                return pair(build.getStatus(), build.getActivity());
+                return teamcity.retrieveLatestBuild(buildType);
             }
         };
     }
--- src/main/java/bad/robot/radiate/teamcity/Build.java
+++ src/main/java/bad/robot/radiate/teamcity/Build.java
@@ -4,6 +4,7 @@ import bad.robot.radiate.Activity;
 import bad.robot.radiate.Hypermedia;
 import bad.robot.radiate.Monitorable;
 import bad.robot.radiate.Status;
+import com.googlecode.totallylazy.Callable1;
 
 import static bad.robot.radiate.Activity.Idle;
 import static bad.robot.radiate.Status.*;
@@ -75,5 +76,26 @@ public class Build extends TeamCityObject implements Hypermedia, Monitorable {
     @Override
     public String getHref() {
         return href;
+    }
+
+    public static class Functions {
+        public static Callable1<Build, Activity> toActivity() {
+            return new Callable1<Build, Activity>() {
+                @Override
+                public Activity call(Build build) throws Exception {
+                    return build.getActivity();
+                }
+            };
+        }
+
+        public static Callable1<Build, Status> toStatus() {
+            return new Callable1<Build, Status>() {
+                @Override
+                public Status call(Build build) throws Exception {
+                    return build.getStatus();
+                }
+            };
+        }
+
     }
 }
