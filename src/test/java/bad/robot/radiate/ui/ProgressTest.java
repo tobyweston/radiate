0b6cc060875ddc3e87e6525f408b6480ce0b24fa
tobyweston <toby.weston@db.com>
2013-11-27 09:22:27 +0000

    improving test

--- src/test/java/bad/robot/radiate/ui/ProgressTest.java
+++ src/test/java/bad/robot/radiate/ui/ProgressTest.java
@@ -9,9 +9,16 @@ import static org.junit.Assert.assertThat;
 public class ProgressTest {
 
     @Test
-    public void stringRepresentation() throws Exception {
+    public void stringRepresentation() {
         assertThat(new Progress(20, 100).toString(), is("20%"));
+        assertThat(new Progress(30, 200).toString(), is("15%"));
         assertThat(new Progress(40, 200).toString(), is("20%"));
         assertThat(new Progress(40, 120).toString(), is("33%"));
+    }
+
+    @Test
+    public void adding() {
+        Progress progress = new Progress(10, 100).add(new Progress(20, 100));
+        assertThat(progress.toString(), is("15%"));
     }
 }
