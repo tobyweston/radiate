056dbbeb22f74c0898eca1ada49ce227e2e652c8
Toby Weston <toby.weston@gmail.com>
2015-05-09 20:34:02 +0100

    adding a few more test cases

--- src/test/scala/bad/robot/radiate/ActivityAggregatorSTest.scala
+++ src/test/scala/bad/robot/radiate/ActivityAggregatorSTest.scala
@@ -18,16 +18,19 @@ class ActivityAggregatorSTest extends Specification {
   "Should be Progressing" >> {
     aggregated(List(Progressing)).getActivity must_== Progressing
     aggregated(List(Idle, Progressing)).getActivity must_== Progressing
+    aggregated(List(Idle, Progressing, Idle)).getActivity must_== Progressing
   }
 
   "Should be Busy" >> {
     aggregated(List(Busy)).getActivity must_== Busy
     aggregated(List(Busy, Idle, Progressing)).getActivity must_== Busy
+    aggregated(List(Busy, Idle, Progressing, Progressing, Idle)).getActivity must_== Busy
   }
 
   "Should be indicating an Error" >> {
     aggregated(List(Error)).getActivity must_== Error
     aggregated(List(Busy, Idle, Error, Progressing)).getActivity must_== Error
+    aggregated(List(Idle, Idle, Progressing, Busy, Error, Idle)).getActivity must_== Error
   }
 }
 
