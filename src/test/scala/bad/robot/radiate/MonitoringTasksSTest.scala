4c2529615bb619a749e2c06e44c5c3c4462cd593
Toby Weston <toby.weston@db.com>
2015-05-11 07:51:58 +0100

    adding a stubbed return type and make the notifyObservers call "ignored"

--- src/test/scala/bad/robot/radiate/MonitoringTasksSTest.scala
+++ src/test/scala/bad/robot/radiate/MonitoringTasksSTest.scala
@@ -1,6 +1,6 @@
 package bad.robot.radiate
 
-import bad.robot.radiate.monitor.{MonitorS, MonitoringTasksFactoryS}
+import bad.robot.radiate.monitor.{MonitoringTaskS, MonitorS, MonitoringTasksFactoryS}
 import org.scalamock.specs2.MockContext
 import org.specs2.mutable.Specification
 
@@ -9,9 +9,10 @@ class MonitoringTasksSTest extends Specification {
   "gathers tasks" in new MockContext {
     val factory = mock[MonitoringTasksFactoryS]
     val monitor = mock[MonitorS]
+    val task = stub[MonitoringTaskS]
 
-    (factory.notifyObservers(_: Exception)).expects(*)
-    (factory.create _).expects().once
+    (factory.notifyObservers(_: Exception)).expects(*).anyNumberOfTimes
+    (factory.create _).expects().once.returning(List(task))
 
     new MonitoringTasksS(factory, monitor)
   }
