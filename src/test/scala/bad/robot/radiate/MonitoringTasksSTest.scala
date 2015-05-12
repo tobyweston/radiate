1402f6672c788afb6dbec3509cfd38c600362a39
Toby Weston <toby.weston@gmail.com>
2015-05-12 20:09:25 +0100

    adding another test

--- src/test/scala/bad/robot/radiate/MonitoringTasksSTest.scala
+++ src/test/scala/bad/robot/radiate/MonitoringTasksSTest.scala
@@ -1,7 +1,8 @@
 package bad.robot.radiate
 
+import java.util.concurrent.ScheduledFuture
+
 import bad.robot.radiate.RestartRequiredS._
-import bad.robot.radiate._
 import bad.robot.radiate.monitor._
 import org.scalamock.specs2.MockContext
 import org.specs2.mutable.Specification
@@ -42,5 +43,21 @@ class MonitoringTasksSTest extends Specification {
     (factory.notifyObservers(_: Exception)).expects(anyTypedOf[Exception, NothingToMonitorExceptionS]).once
 
     new MonitoringTasksS(factory, monitor)
+  }
+
+  "Start and stop" in new MockContext {
+    val tasks = List(new RandomStatusS)
+    val scheduled = List.empty[ScheduledFuture[_]]
+
+    val factory = mock[MonitoringTasksFactoryS]
+    val monitor = mock[MonitorS]
+
+    (factory.create _).expects().returning(tasks).once
+    (monitor.start _).expects(tasks).returning(scheduled).once
+    (monitor.cancel _).expects(scheduled).once
+
+    val monitoring = new MonitoringTasksS(factory, monitor)
+    monitoring.start()
+    monitoring.stop()
   }
 }
