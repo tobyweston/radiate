ff6d643985e7df965aa918fb6cda987fc13c026c
Toby Weston <toby.weston@gmail.com>
2015-05-31 19:36:38 +0100

    weirdness

--- src/main/scala/bad/robot/radiate/teamcity/Projects.scala
+++ src/main/scala/bad/robot/radiate/teamcity/Projects.scala
@@ -3,5 +3,5 @@ package bad.robot.radiate.teamcity
 import com.google.gson.annotations.SerializedName
 
 class ProjectsScala(@SerializedName("project") private val projects: List[ProjectScala]) extends TeamCityObjectS with Iterable[ProjectScala] {
-  def iterator: Iterator[ProjectScala] = projects.iterator
+  def iterator = projects.iterator
 }
