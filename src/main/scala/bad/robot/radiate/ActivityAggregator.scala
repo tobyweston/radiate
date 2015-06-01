7b37764bc1a93be4c7634b7429b71f4d46548188
Toby Weston <toby.weston@db.com>
2015-06-01 07:38:59 +0100

    fixing test

--- src/main/scala/bad/robot/radiate/ActivityAggregator.scala
+++ src/main/scala/bad/robot/radiate/ActivityAggregator.scala
@@ -1,18 +1,14 @@
 package bad.robot.radiate
 
 object ActivityAggregatorS {
-  def aggregated(statuses: List[ActivityS]): ActivityAggregatorS = {
-    new ActivityAggregatorS(statuses)
-  }
+  def aggregated(statuses: List[ActivityS]) = new ActivityAggregatorS(statuses)
 }
 
 class ActivityAggregatorS(activities: List[ActivityS]) {
 
   def getActivity: ActivityS = {
-    if (activities.isEmpty)
-      Idle
-    else
-      activities.reduce[ActivityS] {
+    if (activities.isEmpty) Idle
+    else activities.reduce[ActivityS] {
         case (first, second) if first == Error || second == Error => Error
         case (first, second) if first == Busy || second == Busy => Busy
         case (first, second) if first == Progressing || second == Progressing => Progressing
--- src/main/scala/bad/robot/radiate/StatusAggregator.scala
+++ src/main/scala/bad/robot/radiate/StatusAggregator.scala
@@ -7,10 +7,10 @@ object StatusAggregatorS {
 class StatusAggregatorS(statuses: Iterable[StatusS]) {
 
   def getStatus: StatusS = {
-    statuses.reduce[StatusS] {
+    if (statuses.isEmpty) Unknown
+    else statuses.reduce[StatusS] {
       case (first, second) if first == Broken || second == Broken => Broken
       case (first, second) if first == Unknown || second == Unknown => Unknown
-      case _ if statuses.isEmpty => Unknown
       case _ => Ok;
     }
   }
