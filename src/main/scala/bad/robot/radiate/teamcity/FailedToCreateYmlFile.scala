7d20ac61cb4a98954a3cedfe1fe1f506b8ebe435
Toby Weston <toby.weston@db.com>
2015-08-11 14:53:01 +0100

    rename

--- src/main/scala/bad/robot/radiate/teamcity/FailedToCreateYmlFile.scala
+++ src/main/scala/bad/robot/radiate/teamcity/FailedToCreateYmlFile.scala
@@ -1,14 +1,12 @@
 package bad.robot.radiate.teamcity
 
 import bad.robot.radiate.monitor.Information
-import bad.robot.radiate.teamcity.StringOps._
+import bad.robot.radiate.teamcity.FailedToCreateYmlFile.defaultString
 
 class FailedToCreateYmlFile(e: Exception) extends Information(
   s"Failed to create Yml configuration file (caused by an ${e.getClass.getSimpleName} ${defaultString(e.getMessage)}), falling back to use environment variables"
 )
 
-object StringOps {
-  def defaultString(string: String): String = {
-    if (string == null) "" else string.toString
-  }
+object FailedToCreateYmlFile {
+  def defaultString(string: String): String = if (string == null) "" else string.toString
 }
