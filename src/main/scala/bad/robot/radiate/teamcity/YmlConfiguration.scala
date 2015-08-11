f81e1aab054508130e3ff276b16b016273b12333
Toby Weston <toby.weston@db.com>
2015-08-11 15:37:42 +0100

    SnakeYml doesn't support scala collection types so we have to convert everything to the Java types when loading and writing the Yml config.
    Will prefer to move the config over to [Knobs](http://oncue.github.io/knobs/)

--- src/main/scala/bad/robot/radiate/teamcity/YmlConfiguration.scala
+++ src/main/scala/bad/robot/radiate/teamcity/YmlConfiguration.scala
@@ -24,17 +24,17 @@ object YmlConfiguration {
 }
 
 class YmlConfiguration(file: YmlConfigurationFile) extends TeamCityConfiguration {
-  private val configuration: Map[String, Any] = load(file)
+  private val configuration: java.util.Map[String, Any] = load(file)
 
   @throws(classOf[FileNotFoundException])
-  private def load(configuration: File) = new Yaml().load(new FileReader(configuration)).asInstanceOf[Map[String, Any]]
+  private def load(configuration: File) = new Yaml().load(new FileReader(configuration)).asInstanceOf[java.util.Map[String, Any]]
 
   def host = configuration.get("host").asInstanceOf[String]
 
   def port = configuration.get("port").asInstanceOf[Integer]
 
   def filter(projects: Iterable[Project]) = {
-    val ids = configuration.get("projects").asInstanceOf[List[String]]
+    val ids = configuration.get("projects").asInstanceOf[java.util.List[String]]
     projects.filter(project => ids.contains(project.id))
   }
 
--- src/main/scala/bad/robot/radiate/teamcity/YmlConfigurationFile.scala
+++ src/main/scala/bad/robot/radiate/teamcity/YmlConfigurationFile.scala
@@ -4,6 +4,7 @@ import java.io.{File, IOException}
 
 import org.apache.commons.io.FileUtils.writeStringToFile
 import org.yaml.snakeyaml.Yaml
+import collection.JavaConverters._
 
 class YmlConfigurationFile extends File(System.getProperty("user.home") + File.separator + ".radiate" + File.separator + "config.yml") {
 
@@ -24,13 +25,13 @@ class YmlConfigurationFile extends File(System.getProperty("user.home") + File.s
     try {
       val yaml = new Yaml
       val data = Map[String, Any](
-        "projects" -> getProjectIds(teamcity),
+        "projects" -> getProjectIds(teamcity).asJava,
         "host" -> fallback.host,
         "port" -> fallback.port,
         "user" -> null,
         "password" -> null
       )
-      writeStringToFile(this, yaml.dump(data))
+      writeStringToFile(this, yaml.dump(data.asJava))
     } catch {
       case e: Exception => {
         deleteOnExit()
@@ -45,6 +46,8 @@ class YmlConfigurationFile extends File(System.getProperty("user.home") + File.s
   }
 
   private def createFolder() {
-    new File(System.getProperty("user.home") + File.separator + ".radiate").mkdirs
+    sys.props.get("user.home").map(home => {
+      new File(home + File.separator + ".radiate")
+    }).getOrElse(throw new Exception(s"The system property 'user.home' is not set, this is very strange.")).mkdirs
   }
 }
