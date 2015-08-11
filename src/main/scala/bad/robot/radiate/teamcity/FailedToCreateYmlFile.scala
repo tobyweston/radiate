e26fafdba82645c8242a00b581e7557be6a13695
Toby Weston <toby.weston@db.com>
2015-08-11 14:43:41 +0100

    fixing bug where the embedded message of an exception wasn't displayed (as part of FailedToCreateYmlFile)

--- src/main/scala/bad/robot/radiate/teamcity/FailedToCreateYmlFile.scala
+++ src/main/scala/bad/robot/radiate/teamcity/FailedToCreateYmlFile.scala
@@ -4,11 +4,11 @@ import bad.robot.radiate.monitor.Information
 import bad.robot.radiate.teamcity.StringOps._
 
 class FailedToCreateYmlFile(e: Exception) extends Information(
-    s"Failed to create Yml configuration file (caused by an ${e.getClass.getSimpleName} ${defaultString(e.getMessage)}), falling back to use environment variables"
+  s"Failed to create Yml configuration file (caused by an ${e.getClass.getSimpleName} ${defaultString(e.getMessage)}), falling back to use environment variables"
 )
 
 object StringOps {
-    def defaultString(string: String) {
-        if (string == null) () else string.toString
-    }
+  def defaultString(string: String): String = {
+    if (string == null) "" else string.toString
+  }
 }
