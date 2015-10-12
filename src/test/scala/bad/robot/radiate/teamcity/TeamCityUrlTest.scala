036f7bf2c41d1156c5c212fd5c4c9a79f1aee4b2
Toby Weston <toby.weston@db.com>
2015-10-12 14:47:35 +0100

    fixing test (removing duplicate)

--- src/test/scala/bad/robot/radiate/teamcity/TeamCityUrlTest.scala
+++ src/test/scala/bad/robot/radiate/teamcity/TeamCityUrlTest.scala
@@ -3,8 +3,10 @@ package bad.robot.radiate.teamcity
 import java.net.URL
 
 import bad.robot.radiate.FunctionInterfaceOps.toHypermedia
+import bad.robot.radiate.config.{Authorisation, BasicAuthorisation, GuestAuthorisation}
 import org.specs2.mutable.Specification
 import bad.robot.radiate.UrlSyntax._
+
 class TeamCityUrlTest extends Specification {
 
   "Server host can be supplied with a scheme or without" >> {
@@ -12,8 +14,16 @@ class TeamCityUrlTest extends Specification {
     TeamCityUrl("https://localhost:8080").urlFor("/") must_== new URL("https://localhost:8080/")
   }
 
-  "More tests here" >> {
-    ko("not implemented yet")
+  "Build locator for latest build" >> {
+    val buildType = BuildType("id", "name", "href", "project", "projectId")
+    TeamCityUrl("http://example.com:8111").urlFor(BuildLocatorBuilder.latest(buildType), BasicAuthorisation) must_== new URL("http://example.com:8111/httpAuth/app/rest/builds/buildType:id")
+    TeamCityUrl("http://example.com:8111").urlFor(BuildLocatorBuilder.latest(buildType), GuestAuthorisation) must_== new URL("http://example.com:8111/guestAuth/app/rest/builds/buildType:id")
+  }
+
+  "Build locator for running build" >> {
+    val buildType = BuildType("id", "name", "href", "project", "projectId")
+    TeamCityUrl("http://example.com:8111").urlFor(BuildLocatorBuilder.running(buildType), BasicAuthorisation) must_== new URL("http://example.com:8111/httpAuth/app/rest/builds/buildType:id,running:true")
+    TeamCityUrl("http://example.com:8111").urlFor(BuildLocatorBuilder.running(buildType), GuestAuthorisation) must_== new URL("http://example.com:8111/guestAuth/app/rest/builds/buildType:id,running:true")
   }
 
 }
