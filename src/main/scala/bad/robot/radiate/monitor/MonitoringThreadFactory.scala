27afec7781dca2ea35abae87eccaf167b0f3ed3c
Toby Weston <toby.weston@db.com>
2015-10-20 15:12:51 +0100

    Add qualifier for monitoring threads

--- src/main/scala/bad/robot/radiate/monitor/MonitoringThreadFactory.scala
+++ src/main/scala/bad/robot/radiate/monitor/MonitoringThreadFactory.scala
@@ -6,5 +6,5 @@ import java.util.concurrent.atomic.AtomicInteger
 class MonitoringThreadFactory extends ThreadFactory {
   private val threadCount = new AtomicInteger
 
-  def newThread(runnable: Runnable): Thread = new Thread(runnable, "monitoring-thread-" + threadCount.getAndIncrement)
+  def newThread(runnable: Runnable): Thread = new Thread(runnable, "radiate-monitoring-thread-" + threadCount.getAndIncrement)
 }
